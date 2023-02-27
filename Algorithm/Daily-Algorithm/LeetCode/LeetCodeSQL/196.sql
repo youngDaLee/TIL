f359319abf23ed18d42a9a8df5b25eaa6c8de7bf
YoungDaLee <ldy0956@gmail.com>
2023-02-27 23:02:06 +0900

    230227

--- Algorithm/Daily-Algorithm/LeetCode/LeetCodeSQL/196.sql
+++ Algorithm/Daily-Algorithm/LeetCode/LeetCodeSQL/196.sql
@@ -2,3 +2,13 @@ DELETE t1 FROM Person t1
 JOIN Person t2
 ON t1.email=t2.email
 WHERE t1.id > t2.id;
+
+
+-- -> 이게 더 빠름;
+delete from person
+where id not in  (
+select * from(
+	select min(id)
+    from person
+    group by email) as t
+);
