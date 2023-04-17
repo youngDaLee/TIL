8c8f1820d9462b99e8b8fa5ea25252df138c76b9
youngDaLee <ldy0956@gmail.com>
2023-04-17 22:15:14 +0900

    230417

---
+++ Algorithm/Daily-Algorithm/BOJ/DFS,BFS/달리기.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/16930
+from collections import deque
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    
