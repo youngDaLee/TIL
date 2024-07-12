3596008699be9936551b85b59e85404f49359f30
youngDaLee <ldy0956@gmail.com>
2024-07-12 10:27:47 +0900

    컴포지션api 방식

---
+++ FE/Vuejs/Vue3시작하기/vue-composition/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">change</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+  export default {
+    setup(){
+      // data
+      const message = ref('hello');
+
+      // methods
+      function changeMessage() {
+        message.value = 'hi';
+      }
+
+      return { message, changeMessage }
+    }
+    // data() {
+    //   return {
+    //     message: 'hello'
+    //   }
+    // },
+    // methods: {
+    //   changeMessage() {
+    //     this.message = 'hi'
+    //   }
+    // }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
